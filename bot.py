50fcf8f87ad0656a642ffc53b5d3805e327d13ab
Niteshbots <65546540+Niteshbots@users.noreply.github.com>
2020-08-04 17:13:14 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,69 @@
+from telegram.ext import Filters, MessageHandler, CommandHandler, Updater
+import os
+
+
+
+TOKEN=os.eveniron.get('BOT_TOKEN',None)
+
+updater=Updater(TOKEN,use_context=True)
+
+START_TEXT=os.eveniron.get('Start_Msg',None)
+
+HELP_TEXT=os.eveniron.get('Help_Msg',None)
+
+def start_text(u,c):
+	u.message.reply_text(START_TEXT)
+	
+def help_text(u,c):
+	u.message.reply_text(HELP_TEXT)	
+	
+	
+def frwrd_file(u,c):
+	u.message.reply_document(u.message.document.file_id)
+	
+
+def frwrd_media(u,c):
+	u.message.reply_video(u.message.video.file_id) 
+
+def frwrd_photo(u,c):
+	u.message.reply_photo(u.message.photo[-1].file_id) 
+
+def frwrd_text(u,c):
+	u.message.reply_text(u.message.text) 
+
+
+def frwrd_sticker(u,c):
+	u.message.reply_sticker(u.message.sticker.file_id) 
+
+
+def frwrd_voice(u,c):
+	u.message.reply_voice(u.message.voice.file_id) 
+		
+	
+dp=updater.dispatcher
+
+#Commands
+dp.add_handler(CommandHandler('start',start_text))
+
+#Commands
+dp.add_handler(CommandHandler('help',help_text))
+
+
+dp.add_handler(MessageHandler(Filters.document,frwrd_file))
+
+
+dp.add_handler(MessageHandler(Filters.video,frwrd_media))
+
+dp.add_handler(MessageHandler(Filters.photo,frwrd_photo))
+
+dp.add_handler(MessageHandler(Filters.text,frwrd_text))
+
+dp.add_handler(MessageHandler(Filters.sticker,frwrd_sticker))
+
+dp.add_handler(MessageHandler(Filters.voice,frwrd_voice))
+
+
+#Bot Updating + Starting
+updater.start_polling()
+updater.idle()
+
